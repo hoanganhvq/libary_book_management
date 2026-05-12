8a6bf0efd00055a8fe3b910806b814d56cbe4927
luc12 <danunike687@gmail.com>
2026-05-12 09:49:09 +0700

    Add testcases for getBooks and getBooksById

--- backend/src/main/java/org/example/DTO/BookDTOResponse.java
+++ backend/src/main/java/org/example/DTO/BookDTOResponse.java
@@ -1,8 +1,10 @@
 package org.example.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class BookDTOResponse {
     private Long id;
     private String title;
---
+++ backend/src/test/java/org/example/serviceTest/BookServiceImpleTest.java
@@ -0,0 +1,155 @@
+package org.example.serviceTest;
+
+import org.example.DTO.BookDTOResponse;
+import org.example.Entity.Book;
+import org.example.Exception.ResourceNotFoundException;
+import org.example.Mapper.BookMapper;
+import org.example.Mapper.BookMapperImpl;
+import org.example.Repository.BookRepository;
+import org.example.Service.BookService;
+import org.example.Service.BookServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+@ExtendWith(MockitoExtension.class)
+public class BookServiceImpleTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookServiceImpl bookService;
+    @Mock
+    private BookMapper  bookMapper;
+
+    @Test
+    void getBooks_returnAllBooks() {
+        List<Book> mockBooks = List.of(
+                new Book(1L, "Clean code", "Robert Martin", "Technology", true),
+                new Book(2L, "The Habbit", "Tolkien", "Fiction", true)
+        );
+
+        List<BookDTOResponse> mockResponses = List.of(
+                new BookDTOResponse(1L, "Clean code", "Robert Martin", "Technology",true),
+                new BookDTOResponse(2L, "The Habbit", "Tolkien", "Fiction" ,true)
+        );
+
+        Mockito.when(bookRepository.findAll()).thenReturn(mockBooks);
+
+        when(bookMapper.toResponseList(anyList()))
+                .thenReturn(mockResponses);
+        List<BookDTOResponse> result = bookService.getBooks();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result.get(0).getTitle()).isEqualTo("Clean code");
+        assertThat(result.get(1).getTitle()).isEqualTo("The Habbit");
+        verify(bookRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getBooks_returnEmptyList_whenNoBooksExist() {
+        when(bookRepository.findAll())
+                .thenReturn(Collections.emptyList());
+
+        when(bookMapper.toResponseList(anyList()))
+                .thenReturn(Collections.emptyList());
+
+        List<BookDTOResponse> result = bookService.getBooks();
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void getBooks_throwException_whenRepositoryFails() {
+        when(bookRepository.findAll())
+                .thenThrow(new RuntimeException("DB Error"));
+
+        assertThrows(RuntimeException.class,
+                () -> bookService.getBooks());
+    }
+
+    @Test
+    void getBookById_returnBook_whenBookExists() {
+        Long id = 1L;
+
+        Book mockBook = new Book(
+                1L,
+                "Clean code",
+                "Robert Martin",
+                "Technology",
+                true
+        );
+
+        BookDTOResponse mockResponse =
+                new BookDTOResponse(
+                        1L,
+                        "Clean code",
+                        "Robert Martin",
+                        "Technology",
+                        true
+                );
+
+        when(bookRepository.findById(id))
+                .thenReturn(Optional.of(mockBook));
+
+        when(bookMapper.toDTOResponse(mockBook))
+                .thenReturn(mockResponse);
+
+        BookDTOResponse result =
+                bookService.getBookById(id);
+
+        assertThat(result).isNotNull();
+
+        assertThat(result.getTitle())
+                .isEqualTo("Clean code");
+
+        verify(bookRepository, times(1))
+                .findById(id);
+
+        verify(bookMapper, times(1))
+                .toDTOResponse(mockBook);
+    }
+
+    @Test
+    void getBookById_throwException_whenBookNotFound() {
+        Long id = 99L;
+
+        when(bookRepository.findById(id))
+                .thenReturn(Optional.empty());
+
+        assertThrows(
+                ResourceNotFoundException.class,
+                () -> bookService.getBookById(id)
+        );
+
+        verify(bookRepository, times(1))
+                .findById(id);
+
+        verify(bookMapper, never())
+                .toDTOResponse(any());
+    }
+
+    @Test
+    void getBookById_throwCorrectMessage_whenBookNotFound() {
+        ResourceNotFoundException exception =
+                assertThrows(
+                        ResourceNotFoundException.class,
+                        () -> bookService.getBookById(99L)
+                );
+
+        assertThat(exception.getMessage())
+                .isEqualTo("Book not found");
+    }
+}
